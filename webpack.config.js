850776f1f330e0ad28d66ab9825975f7c4b0ccb1
Christoph.Matscheko <christoph.matscheko@dynatrace.com>
2020-05-28 13:13:37 +0200

    Ensure all files in project are correctly formatted with Prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,13 @@
-const path = require("path");
-const TerserJSPlugin = require("terser-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const path = require('path');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
-  entry: path.join(__dirname, "bookmarklet/bookmarklet.ts"),
+  entry: path.join(__dirname, 'bookmarklet/bookmarklet.ts'),
   output: {
-    filename: "com.dynatrace.timetracking.bookmarklet.js",
-    path: path.resolve(__dirname, "bookmarklet/dist"),
+    filename: 'com.dynatrace.timetracking.bookmarklet.js',
+    path: path.resolve(__dirname, 'bookmarklet/dist'),
   },
   optimization: {
     minimizer: [
@@ -23,14 +23,14 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "com.dynatrace.timetracking.bookmarklet.css",
+      filename: 'com.dynatrace.timetracking.bookmarklet.css',
     }),
   ],
   module: {
     rules: [
       {
         test: /\.ts$/i,
-        loader: "ts-loader",
+        loader: 'ts-loader',
         exclude: /node_modules/,
       },
       {
@@ -38,12 +38,12 @@ module.exports = {
         use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: { sourceMap: true },
           },
         ],
       },
     ],
   },
-  devtool: "source-map",
+  devtool: 'source-map',
 };
